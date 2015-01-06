75c56a67a2734f43adeb5f7596927a641902b432
Lars Greiss <kufikugel@googlemail.com>
2015-01-06 15:49:19 -0500

    Smart Pulldown [2/2]
    Change-Id: I418f6d487d38890812f4c3a46271410e7b37d92c

--- src/com/dirtyunicorns/dutweaks/fragments/QuickSettings.java
+++ src/com/dirtyunicorns/dutweaks/fragments/QuickSettings.java
@@ -44,10 +44,12 @@ public class QuickSettings extends SettingsPreferenceFragment
 
     public static final String TAG = "QuickSettings";
 
-    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
+    private static final String PREF_QUICK_PULLDOWN = "quick_pulldown";
+    private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
     private static final String PREF_BLOCK_ON_SECURE_KEYGUARD = "block_on_secure_keyguard";
 
     ListPreference mQuickPulldown;
+    ListPreference mSmartPulldown;
     SwitchPreference mBlockOnSecureKeyguard;
 
     @Override
@@ -58,9 +60,11 @@ public class QuickSettings extends SettingsPreferenceFragment
 
         PreferenceScreen prefs = getPreferenceScreen();
 
-        mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+        mQuickPulldown = (ListPreference) findPreference(PREF_QUICK_PULLDOWN);
+        mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
         if (!DeviceUtils.isPhone(getActivity())) {
             prefs.removePreference(mQuickPulldown);
+            prefs.removePreference(mSmartPulldown);
         } else {
             // Quick Pulldown
             mQuickPulldown.setOnPreferenceChangeListener(this);
@@ -68,6 +72,13 @@ public class QuickSettings extends SettingsPreferenceFragment
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
             mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
             updateQuickPulldownSummary(statusQuickPulldown);
+
+            // Smart Pulldown
+            mSmartPulldown.setOnPreferenceChangeListener(this);
+            int smartPulldown = Settings.System.getInt(getContentResolver(),
+                    Settings.System.QS_SMART_PULLDOWN, 0);
+            mSmartPulldown.setValue(String.valueOf(smartPulldown));
+            updateSmartPulldownSummary(smartPulldown);
         }
 
         final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
@@ -79,6 +90,7 @@ public class QuickSettings extends SettingsPreferenceFragment
         } else {
             prefs.removePreference(mBlockOnSecureKeyguard);
         }
+
     }
 
     @Override
@@ -95,6 +107,12 @@ public class QuickSettings extends SettingsPreferenceFragment
                     statusQuickPulldown);
             updateQuickPulldownSummary(statusQuickPulldown);
             return true;
+        } else if (preference == mSmartPulldown) {
+            int smartPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.QS_SMART_PULLDOWN,
+                    smartPulldown);
+            updateSmartPulldownSummary(smartPulldown);
+            return true;
         } else if (preference == mBlockOnSecureKeyguard) {
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
@@ -102,6 +120,31 @@ public class QuickSettings extends SettingsPreferenceFragment
             return true;
         }
         return false;
+    }
+
+    private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+        } else {
+            String type = null;
+            switch (value) {
+                case 1:
+                    type = res.getString(R.string.smart_pulldown_dismissable);
+                    break;
+                case 2:
+                    type = res.getString(R.string.smart_pulldown_persistent);
+                    break;
+                default:
+                    type = res.getString(R.string.smart_pulldown_all);
+                    break;
+            }
+            // Remove title capitalized formatting
+            type = type.toLowerCase();
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
+        }
     }
 
     private void updateQuickPulldownSummary(int value) {
@@ -121,4 +164,3 @@ public class QuickSettings extends SettingsPreferenceFragment
     }
 
 }
-
