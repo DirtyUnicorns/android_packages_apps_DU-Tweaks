53d2ee3d20904c0b97e90054ddf9ef0fe2bb1396
Danesh M <daneshm90@gmail.com>
2015-02-17 08:23:22 +0000

    Add screen timeout [2/2]
    Change-Id: Ib599cd6d9ea8b8c252707607374b68d39738f8a1

--- src/com/dirtyunicorns/dutweaks/fragments/QSTileHolder.java
+++ src/com/dirtyunicorns/dutweaks/fragments/QSTileHolder.java
@@ -111,6 +111,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_heads_up_on";
                 stringId = R.string.qs_tile_headsup;
                 break;
+            case QSConstants.TILE_SCREEN_TIMEOUT:
+                resourceName = "ic_qs_screen_timeout_vector";
+                stringId = R.string.qs_tile_screen_timeout;
+                break;
             default:
                 return null;
         }
