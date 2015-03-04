4ad0301d08069489156df6cfefd7d77e3e7f842e
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-03-04 00:55:48 +0000

    Reboot/Recovery tile (2/2)
    Change-Id: I264e48580da4446607dfaa637df7c705461c5abe

--- src/com/dirtyunicorns/dutweaks/fragments/QSTileHolder.java
+++ src/com/dirtyunicorns/dutweaks/fragments/QSTileHolder.java
@@ -103,6 +103,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_title_screenshot;
                 break;
+            case QSConstants.TILE_REBOOT:
+                resourceName = "ic_qs_reboot";
+                stringId = R.string.qs_tile_reboot;
+                break;
             case QSConstants.TILE_SYNC:
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_title_sync;
