23a6b99a2028654bacbcc0dec58a3229a958f3d4
Thecrazyskull <anaskarbila@gmail.com>
2015-07-07 10:01:57 +0200

    Fix the empty carrier label option on msim devices
    Change-Id: I3fad950ac92cf1ffaf34726f3114c853000ce286
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>

--- src/com/dirtyunicorns/dutweaks/fragments/CarrierLabel.java
+++ src/com/dirtyunicorns/dutweaks/fragments/CarrierLabel.java
@@ -85,13 +85,7 @@ public class CarrierLabel extends SettingsPreferenceFragment implements OnPrefer
         mCarrierColorPicker.setSummary(hexColor);
         mCarrierColorPicker.setNewPreviewColor(intColor);
 
-        if (TelephonyManager.getDefault().isMultiSimEnabled()) {
-            prefSet.removePreference(mStatusBarCarrier);
-            prefSet.removePreference(mCustomCarrierLabel);
-        } else {
             updateCustomLabelTextSummary();
-        }
-
     }
 
     private void updateCustomLabelTextSummary() {
