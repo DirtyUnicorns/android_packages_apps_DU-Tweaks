28409e69cf02b97f105612fbcbb3762eb5a77828
LorDClockaN <davor@losinj.com>
2015-07-22 08:26:28 +0000

    DU logo color settings [2/2]
    Change-Id: I09b5003bb02bca2efe8a3ed4736a3c2a2435e882

---
+++ src/com/dirtyunicorns/dutweaks/fragments/DuLogo.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.dutweaks.fragments;
+
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class DuLogo extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String TAG = "DuLogo";
+
+    private static final String KEY_DU_LOGO_COLOR = "status_bar_du_logo_color";
+
+    private ColorPickerPreference mDuLogoColor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.du_logo);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        // DU logo color
+        mDuLogoColor =
+            (ColorPickerPreference) prefSet.findPreference(KEY_DU_LOGO_COLOR);
+        mDuLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_DU_LOGO_COLOR, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mDuLogoColor.setSummary(hexColor);
+            mDuLogoColor.setNewPreviewColor(intColor);
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mDuLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_DU_LOGO_COLOR, intHex);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+}
