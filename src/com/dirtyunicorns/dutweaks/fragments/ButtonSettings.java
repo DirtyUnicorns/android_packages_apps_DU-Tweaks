1a8889fa60bd8a426bac37aa3bc3b62df0143ba2
rascarlo <rascarlo@gmail.com>
2015-12-05 15:06:27 +0000

    Move Volume Rocker to Dirty Tweaks [2/2]
    Change-Id: Ib83943373ad5dabeda65466b3d4b84eb5aa3e61e

--- src/com/dirtyunicorns/dutweaks/fragments/ButtonSettings.java
+++ src/com/dirtyunicorns/dutweaks/fragments/ButtonSettings.java
@@ -64,10 +64,11 @@ import com.android.internal.logging.MetricsLogger;
 
 public class ButtonSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    // volume rocker reorient
     private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
     private SwitchPreference mSwapVolumeButtons;
+    private SwitchPreference mVolumeRockerWake;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -79,12 +80,17 @@ public class ButtonSettings extends SettingsPreferenceFragment implements OnPref
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
 
-        // volume rocker reorient
         mSwapVolumeButtons = (SwitchPreference) findPreference(SWAP_VOLUME_BUTTONS);
         mSwapVolumeButtons.setOnPreferenceChangeListener(this);
         int swapVolumeButtons = Settings.System.getInt(getContentResolver(),
                 SWAP_VOLUME_BUTTONS, 0);
         mSwapVolumeButtons.setChecked(swapVolumeButtons != 0);
+
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
     }
 
     @Override
@@ -102,6 +108,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements OnPref
         if (preference == mSwapVolumeButtons) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), SWAP_VOLUME_BUTTONS,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
                     value ? 1 : 0);
             return true;
         }
