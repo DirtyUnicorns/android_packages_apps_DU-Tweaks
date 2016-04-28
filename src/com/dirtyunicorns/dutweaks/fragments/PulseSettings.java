16bc8cfcb0df46939ccd85d6edee1b52955af1ac
Varun Date <date.varun123@gmail.com>
2016-04-27 20:11:02 -0400

    [Squashed] Dyanamically Configurable Pulse Bar Dimensions [3/3]
    **DU Edits**
    
    - Move to Secure to avoid apps from messing with things as per Randall-
    
    Change-Id: I0857c952cb614521785a85fb934cb6cf47734b26
    
    [Squashed] DuTweaks:Add Pulse Configs [3/3]
    
    --Pulse Bar Spacing
    --Pulse Visualizer Block Size
    --Pulse Visualzier Empty Block Spacing
    --Pulse Visualizer Sanity Level
    --Add Description and Summaries
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    -Clean Up
    
    Change-Id: I0857c952cb614521785a85fb934cb6cf47734b29
    
    DUify the advanced pulse settings
    
    Change-Id: I443d59b1b11a12f425cfc4ffa6090fe6cd4eecb8

--- src/com/dirtyunicorns/dutweaks/fragments/PulseSettings.java
+++ src/com/dirtyunicorns/dutweaks/fragments/PulseSettings.java
@@ -30,15 +30,27 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.SwitchPreference;
 import android.preference.Preference;
+import android.preference.ListPreference;
 import android.provider.Settings;
 
 public class PulseSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
     private static final String TAG = PulseSettings.class.getSimpleName();
+    private static final String CUSTOM_DIMEN = "pulse_custom_dimen";
+    private static final String CUSTOM_DIV = "pulse_custom_div";
+    private static final String PULSE_BLOCK = "pulse_filled_block_size";
+    private static final String EMPTY_BLOCK = "pulse_empty_block_size";
+    private static final String FUDGE_FACOR = "pulse_custom_fudge_factor";
 
     SwitchPreference mShowPulse;
     SwitchPreference mLavaLampEnabled;
     ColorPickerPreference mPulseColor;
+    ListPreference mCustomDimen;
+    ListPreference mCustomDiv;
+    ListPreference mFilled;
+    ListPreference mEmpty;
+    ListPreference mFudge;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,6 +77,36 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         mLavaLampEnabled.setChecked(Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, 1) == 1);
         mLavaLampEnabled.setOnPreferenceChangeListener(this);
+
+        int customdimen = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_CUSTOM_DIMEN, 0, UserHandle.USER_CURRENT);
+        mCustomDimen = (ListPreference) findPreference(CUSTOM_DIMEN);
+        mCustomDimen.setValue(String.valueOf(customdimen));
+        mCustomDimen.setOnPreferenceChangeListener(this);
+
+        int customdiv = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_CUSTOM_DIV, 0, UserHandle.USER_CURRENT);
+        mCustomDiv = (ListPreference) findPreference(CUSTOM_DIV);
+        mCustomDiv.setValue(String.valueOf(customdiv));
+        mCustomDiv.setOnPreferenceChangeListener(this);
+
+        int filled = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_FILLED_BLOCK_SIZE, 0, UserHandle.USER_CURRENT);
+        mFilled = (ListPreference) findPreference(PULSE_BLOCK);
+        mFilled.setValue(String.valueOf(filled));
+        mFilled.setOnPreferenceChangeListener(this);
+
+        int empty = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_EMPTY_BLOCK_SIZE, 0, UserHandle.USER_CURRENT);
+        mEmpty = (ListPreference) findPreference(EMPTY_BLOCK);
+        mEmpty.setValue(String.valueOf(empty));
+        mEmpty.setOnPreferenceChangeListener(this);
+
+        int fudge = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_CUSTOM_FUDGE_FACTOR, 0, UserHandle.USER_CURRENT);
+        mFudge = (ListPreference) findPreference(FUDGE_FACOR);
+        mFudge.setValue(String.valueOf(fudge));
+        mFudge.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -83,6 +125,31 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             boolean enabled = ((Boolean) newValue).booleanValue();
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, enabled ? 1 : 0);
+            return true;
+        } else if (preference == mCustomDimen) {
+            int customdimen = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_CUSTOM_DIMEN, customdimen, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mCustomDiv) {
+            int customdiv = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_CUSTOM_DIV, customdiv, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mFilled) {
+            int filled = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_FILLED_BLOCK_SIZE, filled, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mEmpty) {
+            int empty = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_EMPTY_BLOCK_SIZE, empty, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mFudge) {
+            int fudge = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_CUSTOM_FUDGE_FACTOR, fudge, UserHandle.USER_CURRENT);
             return true;
         }
         return false;
