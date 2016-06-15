c8ea0d43e3f2a60232b7de859e0229c6b75f6abd
GodfatherX64 <godfather.x64@gmail.com>
2016-06-15 12:51:05 +0200

    Enable single tap rebinding for hwkey back and home
    Initially, this policy was put in place to safeguard
    the UX by preventing a usecase where the device could
    be put in a state without back or home available. However,
    if a user puts the device in this state, it is easy enough
    to navigate to button settings to fix it.
    
    Change-Id: I6b71f9e54fcb99356e07f236e9ed926ac5fc017e

--- src/com/dirtyunicorns/dutweaks/fragments/ButtonSettings.java
+++ src/com/dirtyunicorns/dutweaks/fragments/ButtonSettings.java
@@ -145,6 +145,11 @@ public class ButtonSettings extends ActionFragment implements OnPreferenceChange
     }
 
     @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
+    }
+
+    @Override
     protected int getMetricsCategory() {
         return MetricsEvent.DIRTYTWEAKS;
     }
