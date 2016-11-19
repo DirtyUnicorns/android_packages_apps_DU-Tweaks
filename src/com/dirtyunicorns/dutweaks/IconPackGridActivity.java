23f8e1d4f9bc6ed588cba0217a05e791005340fa
Alex Cruz <mazdarider23@gmail.com>
2016-11-19 05:28:01 +0100

    Fix possible NPE
    Change-Id: I7067c44948cd2d28d7b8a8dd402d9fc97f947a4a

--- src/com/dirtyunicorns/dutweaks/IconPackGridActivity.java
+++ src/com/dirtyunicorns/dutweaks/IconPackGridActivity.java
@@ -109,7 +109,10 @@ public class IconPackGridActivity extends Activity {
             finish();
         }
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         setContentView(R.layout.icon_picker_grid);
 
         // Get memory class of this device, exceeding this amount will throw an
