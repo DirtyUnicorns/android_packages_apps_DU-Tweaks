0932716e16d76ab7d0e55a1b7083d171090e7ccd
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-25 09:17:55 -0500

    Battery icon: let disable force text outside icon when charging [2/2]
    Change-Id: I531aa7f1a10b29691cd2171a82445b85eb4b3bc3

--- src/com/dirtyunicorns/dutweaks/fragments/StatusbarBatteryStyle.java
+++ src/com/dirtyunicorns/dutweaks/fragments/StatusbarBatteryStyle.java
@@ -57,6 +57,7 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_BATTERY_STYLE_TILE = "status_bar_battery_style_tile";
     private static final String STATUS_BAR_CHARGE_COLOR = "status_bar_charge_color";
+    private static final String FORCE_CHARGE_BATTERY_TEXT = "force_charge_battery_text";
 
     private static final int STATUS_BAR_BATTERY_STYLE_PORTRAIT = 0;
 
@@ -69,6 +70,7 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
     private int mStatusBarBatteryValue;
     private int mStatusBarBatteryShowPercentValue;
     private SwitchPreference mQsBatteryTitle;
+    private SwitchPreference mForceChargeBatteryText;
 
     @Override
     protected int getMetricsCategory() {
@@ -87,6 +89,11 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
         mQsBatteryTitle.setChecked((Settings.Secure.getInt(resolver,
                 Settings.Secure.STATUS_BAR_BATTERY_STYLE_TILE, 1) == 1));
         mQsBatteryTitle.setOnPreferenceChangeListener(this);
+
+        mForceChargeBatteryText = (SwitchPreference) findPreference(FORCE_CHARGE_BATTERY_TEXT);
+        mForceChargeBatteryText.setChecked((Settings.Secure.getInt(resolver,
+                Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, 1) == 1));
+        mForceChargeBatteryText.setOnPreferenceChangeListener(this);
 
         mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
         mStatusBarBatteryValue = Settings.Secure.getInt(resolver,
@@ -109,7 +116,7 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
         mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
         mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
 
-        enableStatusBarBatteryDependents(mStatusBarBatteryValue);
+        enableStatusBarBatteryDependents(mStatusBarBatteryValue, mStatusBarBatteryShowPercentValue);
     }
 
     @Override
@@ -128,7 +135,6 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
                     mStatusBarBattery.getEntries()[index]);
             Settings.Secure.putInt(resolver,
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, mStatusBarBatteryValue);
-            enableStatusBarBatteryDependents(mStatusBarBatteryValue);
         } else if (preference == mStatusBarBatteryShowPercent) {
             mStatusBarBatteryShowPercentValue = Integer.valueOf((String) newValue);
             int index = mStatusBarBatteryShowPercent.findIndexOfValue((String) newValue);
@@ -140,28 +146,37 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
             boolean checked = ((SwitchPreference)preference).isChecked();
             Settings.Secure.putInt(resolver,
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE_TILE, checked ? 1:0);
+        } else if  (preference == mForceChargeBatteryText) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, checked ? 1:0);
         } else if (preference.equals(mChargeColor)) {
             int color = ((Integer) newValue).intValue();
             Settings.Secure.putInt(resolver,
                     Settings.Secure.STATUS_BAR_CHARGE_COLOR, color);
             return true;
         }
+        enableStatusBarBatteryDependents(mStatusBarBatteryValue, mStatusBarBatteryShowPercentValue);
 
         return true;
     }
 
-    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+    private void enableStatusBarBatteryDependents(int batteryIconStyle, int showPercentMode) {
         if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_HIDDEN ||
                 batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
             mStatusBarBatteryShowPercent.setEnabled(false);
             mQsBatteryTitle.setEnabled(false);
+            mForceChargeBatteryText.setEnabled(false);
             mChargeColor.setEnabled(false);
         } else if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_PORTRAIT) {
+            mStatusBarBatteryShowPercent.setEnabled(true);
             mQsBatteryTitle.setEnabled(false);
             mChargeColor.setEnabled(true);
+            mForceChargeBatteryText.setEnabled(showPercentMode == 1 ? true : false);
         } else {
             mStatusBarBatteryShowPercent.setEnabled(true);
             mQsBatteryTitle.setEnabled(true);
+            mForceChargeBatteryText.setEnabled(showPercentMode == 1 ? true : false);
             mChargeColor.setEnabled(true);
         }
     }
