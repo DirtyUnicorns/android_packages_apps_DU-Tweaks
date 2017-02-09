8f7e7c4c858db4ab606f035daa9c089f725ca00e
Alex Cruz <mazdarider23@gmail.com>
2017-02-09 05:14:29 +0100

    Hide Lockscreen emergency button [2/2]
    Change-Id: Id487314bd5c6b1243d0e46f248dee28e9f5c51cb

--- src/com/dirtyunicorns/dutweaks/fragments/LockItems.java
+++ src/com/dirtyunicorns/dutweaks/fragments/LockItems.java
@@ -21,20 +21,27 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
-import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
-import com.android.settings.R;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.widget.LockPatternUtils;
+
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 public class LockItems extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String PREF_SHOW_EMERGENCY_BUTTON = "show_emergency_button";
+
+    private SwitchPreference mEmergencyButton;
+
+    private static final int MY_USER_ID = UserHandle.myUserId();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,17 @@ public class LockItems extends SettingsPreferenceFragment implements OnPreferenc
         addPreferencesFromResource(R.xml.lock_items);
 
         final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
+
+        mEmergencyButton = (SwitchPreference) findPreference(PREF_SHOW_EMERGENCY_BUTTON);
+        if (lockPatternUtils.isSecure(MY_USER_ID)) {
+            mEmergencyButton.setChecked((Settings.System.getInt(resolver,
+                Settings.System.SHOW_EMERGENCY_BUTTON, 1) == 1));
+            mEmergencyButton.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mEmergencyButton);
+        }
     }
 
     @Override
@@ -55,7 +73,15 @@ public class LockItems extends SettingsPreferenceFragment implements OnPreferenc
         super.onResume();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object value) {
-         return true;
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        if  (preference == mEmergencyButton) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_EMERGENCY_BUTTON, checked ? 1:0);
+            return true;
+        }
+        return false;
     }
 }
