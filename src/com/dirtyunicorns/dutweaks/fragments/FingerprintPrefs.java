89dd0115fb3edac65d979fbe3c7cbeac931e3d17
Alex Cruz <du.alexcruz@gmail.com>
2017-05-03 12:18:56 +0200

    Move fingerprint preferences into own fragment
    - Also, let's move lockscreen media to lockscreen items as it fits cleaner there.
    
    - Fix themes once and for our while exposing colors for tabs/text in 
    Dirty Tweaks. All thanks to @daveyannihilation 
    
    Change-Id: Ia8de4ec484e61f3de84f4523a45d4e7b261d1f10

--- src/com/dirtyunicorns/dutweaks/fragments/FingerprintPrefs.java
+++ src/com/dirtyunicorns/dutweaks/fragments/FingerprintPrefs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ * Copyright (C) 2014-2017 The Dirty Unicorns Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.dirtyunicorns.dutweaks.tabs;
+package com.dirtyunicorns.dutweaks.fragments;
 
 import android.content.Context;
 import android.content.ContentResolver;
@@ -24,47 +24,40 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
-import android.hardware.fingerprint.FingerprintManager;
-
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.util.du.DuUtils;
 
-public class Lockscreen extends SettingsPreferenceFragment implements
+import com.dirtyunicorns.dutweaks.preference.SystemSettingSwitchPreference;
+
+public class FingerprintPrefs extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
-    private static final String FINGERPRINT_PREFS = "fingerprint_prefs";
+    private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
 
-    private SwitchPreference mKeyguardTorch;
-    private PreferenceScreen mFingerprintPrefs;
-
-    private FingerprintManager mFingerprintManager;
+    private SystemSettingSwitchPreference mFpKeystore;
+    private SystemSettingSwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.lockscreen);
+        addPreferencesFromResource(R.xml.fingerprint_prefs);
         PreferenceScreen prefSet = getPreferenceScreen();
 
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mKeyguardTorch = (SwitchPreference) findPreference(KEYGUARD_TOGGLE_TORCH);
-        mKeyguardTorch.setOnPreferenceChangeListener(this);
-        if (!DuUtils.deviceSupportsFlashLight(getActivity())) {
-            prefSet.removePreference(mKeyguardTorch);
-        } else {
-            mKeyguardTorch.setChecked((Settings.System.getInt(resolver,
-                    Settings.System.KEYGUARD_TOGGLE_TORCH, 0) == 1));
-        }
+        mFpKeystore = (SystemSettingSwitchPreference) findPreference(FP_UNLOCK_KEYSTORE);
+        mFpKeystore.setChecked((Settings.System.getInt(getContentResolver(),
+               Settings.System.FP_UNLOCK_KEYSTORE, 0) == 1));
+        mFpKeystore.setOnPreferenceChangeListener(this);
 
-        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-        mFingerprintPrefs = (PreferenceScreen) findPreference(FINGERPRINT_PREFS);
-        if (!mFingerprintManager.isHardwareDetected()){
-            prefSet.removePreference(mFingerprintPrefs);
-        }
+        mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+        mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.FINGERPRINT_SUCCESS_VIB, 0) == 1));
+        mFingerprintVib.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -83,10 +76,15 @@ public class Lockscreen extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if  (preference == mKeyguardTorch) {
-            boolean checked = ((SwitchPreference)preference).isChecked();
+        if (preference == mFpKeystore) {
+            boolean value = (Boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.KEYGUARD_TOGGLE_TORCH, checked ? 1:0);
+                    Settings.System.FP_UNLOCK_KEYSTORE, value ? 1 : 0);
+            return true;
+        } else if (preference == mFingerprintVib) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
             return true;
         }
         return false;
