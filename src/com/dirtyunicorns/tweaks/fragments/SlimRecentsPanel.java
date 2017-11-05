76551bee2ce9f4ac343579860404f5aad9b59c9e
ezio84 <brabus84@gmail.com>
2017-11-05 17:19:56 +0100

    Set fast mode (never expanded) as default
    Change-Id: Id10bf645d1cb86a325fe4753b8e26eebb3a114a5

--- src/com/dirtyunicorns/tweaks/fragments/SlimRecentsPanel.java
+++ src/com/dirtyunicorns/tweaks/fragments/SlimRecentsPanel.java
@@ -196,6 +196,9 @@ public class SlimRecentsPanel extends SettingsPreferenceFragment implements
             int value = Integer.parseInt((String) newValue);
             Settings.System.putInt(getContext().getContentResolver(),
                     Settings.System.RECENT_PANEL_EXPANDED_MODE, value);
+            int index = mRecentPanelExpandedMode.findIndexOfValue((String) newValue);
+            mRecentPanelExpandedMode.setSummary(
+                    mRecentPanelExpandedMode.getEntries()[index]);
             return true;
         } else if (preference == mRecentPanelBgColor) {
             String hex = ColorPickerPreference.convertToARGB(
@@ -251,8 +254,9 @@ public class SlimRecentsPanel extends SettingsPreferenceFragment implements
         mRecentPanelScale.setValue(recentScale);
 
         final int recentExpandedMode = Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.RECENT_PANEL_EXPANDED_MODE, 0);
+                Settings.System.RECENT_PANEL_EXPANDED_MODE, 1);
         mRecentPanelExpandedMode.setValue(recentExpandedMode + "");
+        mRecentPanelExpandedMode.setSummary(mRecentPanelExpandedMode.getEntry());
     }
 
     private void initializeAllPreferences() {
