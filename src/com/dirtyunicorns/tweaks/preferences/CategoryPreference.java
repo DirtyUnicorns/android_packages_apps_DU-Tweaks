2f612e3ac38fff85d91c409e73c1f19374d68d80
Alex Cruz <du.alexcruz@gmail.com>
2018-02-15 19:10:15 -0500

    Fruity pebbles [3/4]
    Although not required because this doesn't use any of OnePlus' code,
    I want to say thanks to OnePlus for the inspiration behind this layout.
    
    I think is among the best out there and that's why I wanted to mirror
    it for our Accent picker :-)
    
    - Added condition for DU-Tweaks preference icons to be themed a different
    color for the dark theme. This was an issue with the dark theme while using
    the white accent. The white preference background with white icons wasn't
    working out so well.
    
    Change-Id: I577d84b40f7515cb4638c31c2a8a5ab28051ef3e

--- src/com/dirtyunicorns/tweaks/preferences/CategoryPreference.java
+++ src/com/dirtyunicorns/tweaks/preferences/CategoryPreference.java
@@ -17,8 +17,14 @@
 package com.dirtyunicorns.tweaks.preferences;
 
 import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
 import android.support.annotation.VisibleForTesting;
 import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.preference.Preference;
@@ -42,8 +48,13 @@ public class CategoryPreference extends Preference {
     private boolean mAllowDividerAbove;
     private boolean mAllowDividerBelow;
 
+    private IOverlayManager mOverlayManager;
+
     public CategoryPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Preference);
 
@@ -70,6 +81,20 @@ public class CategoryPreference extends Preference {
         holder.itemView.setClickable(selectable);
         holder.setDividerAllowedAbove(mAllowDividerAbove);
         holder.setDividerAllowedBelow(mAllowDividerBelow);
+
+        ImageView imageview = (ImageView) holder.findViewById(android.R.id.icon);
+        imageview.getDrawable().setColorFilter(getContext().getResources().getColor(
+                isUsingWhiteAccent() ? R.color.BottomBarBackgroundColor : android.R.color.white), PorterDuff.Mode.SRC_IN);
     }
 
+    private boolean isUsingWhiteAccent() {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = mOverlayManager.getOverlayInfo("com.accents.white",
+                    UserHandle.USER_CURRENT);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
 }
