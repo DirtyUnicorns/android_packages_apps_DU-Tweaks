c1582819df77aa392abd81361ba13445a1c0b3ae
Alex <du.alexcruz@gmail.com>
2019-02-10 20:36:23 -0600

    Quick settings: Make animation preferences consistent
    * For preferences not in use; we disable them, not deselect them.
    
    Change-Id: Icae427d9d27dbd87395ba2f5d2f61c2f7510ba24

--- src/com/dirtyunicorns/tweaks/fragments/QuickSettings.java
+++ src/com/dirtyunicorns/tweaks/fragments/QuickSettings.java
@@ -200,11 +200,11 @@ public class QuickSettings extends SettingsPreferenceFragment
     private void updateAnimTileStyle(int tileAnimationStyle) {
         if (mTileAnimationDuration != null) {
             if (tileAnimationStyle == 0) {
-                mTileAnimationDuration.setSelectable(false);
-                mTileAnimationInterpolator.setSelectable(false);
+                mTileAnimationDuration.setEnabled(false);
+                mTileAnimationInterpolator.setEnabled(false);
             } else {
-                mTileAnimationDuration.setSelectable(true);
-                mTileAnimationInterpolator.setSelectable(true);
+                mTileAnimationDuration.setEnabled(true);
+                mTileAnimationInterpolator.setEnabled(true);
             }
         }
     }
