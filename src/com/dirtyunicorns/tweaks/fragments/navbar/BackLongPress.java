1d824f259637000416e54a1fd7fe8440b9bf005e
Alex <du.alexcruz@gmail.com>
2019-02-18 16:32:55 -0600

    Navbar: Add custom app actions [2/3]
    Change-Id: I96e83eae758d82136ab135e05793cc262e42d0b0

---
+++ src/com/dirtyunicorns/tweaks/fragments/navbar/BackLongPress.java
@@ -0,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class BackLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}
